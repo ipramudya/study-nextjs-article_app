fbb2c5a2faec19d18fdbf17b314b09f13809f54e
ipramudya <theprimedownload@gmail.com>
2022-01-07 14:09:39 +0700

    Adding navbar and layouting

---
+++ components/Layout.js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+
+import Nav from "./Nav";
+import styles from "../styles/Layout.module.css";
+
+export default function Layout({ children, title, description }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content={description} />
+        <link rel="preload" href="/fonts/BasementGrotesque/BasementGrotesque-Black_v1.202.otf" as="font" />
+        <link rel="preload" href="/fonts/Aileron/Aileron-Regular.otf" as="font" />
+        <link rel="preload" href="/fonts/Aileron/Aileron-SemiBold.otf" as="font" />
+        <link rel="preload" href="/fonts/Aileron/Aileron-Bold.otf" as="font" />
+        <title>{title}</title>
+      </Head>
+      <div className={styles.main}>
+        <Nav />
+        <div className={styles.content}>{children}</div>
+      </div>
+    </>
+  );
+}
+
+Layout.defaultProps = {
+  title: "Minimalism Article ~ Write your wonderful thoughs",
+  description: "Article site for sharing thoughs to each other",
+};
---
+++ components/Nav.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import styles from "../styles/Nav.module.css";
+
+export default function Nav() {
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.nav_inner}>
+        <div className={styles.nav_logo}>
+          <Link href="/">
+            <a className={styles.logo}>word by word</a>
+          </Link>
+        </div>
+        <ul className={styles.nav_links}>
+          <li className={styles.nav_item}>
+            <Link href="/items">articles</Link>
+          </li>
+          <li className={styles.nav_item}>
+            <Link href="/about">about</Link>
+          </li>
+          <li className={styles.nav_item}>
+            <Link href="/login">login</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
